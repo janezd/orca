d13fabae5f226348a826ff0388db1f52e84c95bb
janezd <janez.demsar@fri.uni-lj.si>
2014-01-14 00:26:39 +0100

    added documentation

--- R/orca.R
+++ R/orca.R
@@ -13,27 +13,27 @@ convert.graph <- function(graph) {
 count4 <- function(graph) {
     edges <- convert.graph(graph) 
     .C("count4",
-       edges, dim(edges),
-       orbits=matrix(0L, nrow=max(edges), ncol=15))$orbits
+        edges, dim(edges),
+        orbits=matrix(0L, nrow=max(edges), ncol=15))$orbits
 }
 
-count5 <- function(edges) {
-    edges <- t(data.matrix(edges))
+count5 <- function(graph) {
+    edges <- convert.graph(graph) 
     .C("count5",
-       edges, dim(edges),
-       orbits=matrix(0L, nrow=max(edges), ncol=73))$orbits
+        edges, dim(edges),
+        orbits=matrix(0L, nrow=max(edges), ncol=73))$orbits
 }
 
-ecount4 <- function(edges) {
-    edges <- t(data.matrix(edges))
+ecount4 <- function(graph) {
+    edges <- convert.graph(graph) 
     .C("ecount4",
-       edges, dim(edges),
-       orbits=matrix(0L, nrow=ncol(edges), ncol=12))$orbits
+        edges, dim(edges),
+        orbits=matrix(0L, nrow=ncol(edges), ncol=12))$orbits
 }
 
-ecount5 <- function(edges) {
-    edges <- t(data.matrix(edges))
+ecount5 <- function(graph) {
+    edges <- convert.graph(graph) 
     .C("ecount5",
-       edges, dim(edges),
-       orbits=matrix(0L, nrow=ncol(edges), ncol=68))$orbits
+        edges, dim(edges),
+        orbits=matrix(0L, nrow=ncol(edges), ncol=68))$orbits
 }
